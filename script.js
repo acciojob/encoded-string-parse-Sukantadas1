50c7edeb3e22cbc73cc445106724041f99ab7dbb
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-29 10:44:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const parseCode = (str) => {
-  // your code here
+  // Split the encoded string using "000" as the separator
+  const parts = encodedString.split("000");
+
+  // Extract the values for firstName, lastName, and id
+  const firstName = parts[0];
+  const lastName = parts[1];
+  const id = parts[2];
+
+  // Create and return an object with the extracted values
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    id: id
+  };
 };
 
 // Do not change the code below
